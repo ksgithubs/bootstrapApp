cef74e4775b37cba14195d7f955b0de0ba4de36a
2139792 <k@gmail.com>
2023-06-22 10:54:08 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LicenseComponent } from './license/license.component';
+import { MembersComponent } from './members/members.component';
+import { TemplateComponent } from './template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:"home",component:HomeComponent
+},{
+  path:"template",component:TemplateComponent
+},{
+  path:"license",component:LicenseComponent
+},{
+  path:"contact",component:ContactComponent
+},
+{
+  path:"members",component: MembersComponent
+},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routing = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TemplateComponent } from './template/template.component';
+import { LicenseComponent } from './license/license.component';
+import { ContactComponent } from './contact/contact.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TemplateComponent,
+    LicenseComponent,
+    ContactComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
